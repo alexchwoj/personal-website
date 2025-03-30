196ef65d6aae36e15f8889a87ed29d0d575ed8b7
alexchwoj <alexanderchwojewski@gmail.com>
2025-03-30 15:27:52 -0300

    Add grid

--- src/lib/components/RotatingObject.svelte
+++ src/lib/components/RotatingObject.svelte
@@ -1,12 +1,8 @@
 <script>
 	import { T, useTask } from '@threlte/core';
 	import { Grid, interactivity, useGltf } from '@threlte/extras';
-	import { spring } from 'svelte/motion';
-
-	import vertexShader from '$lib/shaders/vertex.glsl?raw';
 
 	interactivity();
-	const scale = spring(2);
 	let rotation = 0;
 	useTask((delta) => {
 		rotation += delta;
@@ -34,13 +30,24 @@
 <T.PointLight intensity={50} position={[-4, 1, 4]} color="#8700FF" />
 <T.PointLight intensity={50} position={[0, 6, 0]} color="#00FFAA" />
 
+<Grid
+    position.y={-2}
+    gridSize={[40, 40]}
+    cellSize={1}
+    cellThickness={0.5}
+    cellColor="#6f6f6f"
+    sectionSize={5}
+    sectionThickness={1}
+    sectionColor="#6f6f6f"
+    fadeDistance={32}
+    fadeStrength={1}
+/>
+
 {#await useGltf('/models/original_angel.glb') then angel_old_marble_version}
 	<T
 		is={angel_old_marble_version.scene}
 		position={[0, -2, 0]}
 		scale={0.8}
 		rotation.y={rotation}
-		on:pointerenter={() => scale.set(2.5)}
-		on:pointerleave={() => scale.set(2)}
 	/>
 {/await}
